f66f658afbddea3262ee48b00aaed4e8ec2e1af0
ThinhSuycode <thinhcound96@gmail.com>
2026-08-17 17:36:58 +0700

    fix file

--- autocar-frontend/src/components/HeaderDate/HeaderDate.tsx
+++
@@ -1,9 +0,0 @@
-import classNames from "classnames/bind";
-import styles from "./HeaderDate.module.scss";
-
-const cx = classNames.bind(styles);
-const HeaderDate = () => {
-  return <div></div>;
-};
-
-export default HeaderDate;
